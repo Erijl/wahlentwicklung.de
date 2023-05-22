01b8f725d0280580a7c829634c86ba8caad20ca8
Erijl <justusmau@web.de>
2023-05-22 20:29:47 +0200

    init

---
+++ wahlentwicklung-website/src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<h1 class="text-3xl font-bold">
+    Wahlentwicklung
+    <br>
+
+    Detaillierte Stastiken zur Entwicklung der Bundestagswahl in Deutschland seid 1976!
+  </h1>
+  
+  <style lang="postcss">
+    :global(html) {
+      background-color: theme(colors.gray.100);
+    }
+  </style>

c93c03fd35e2fd5da451eceb76e9bbc1df293e6b
Erijl <justusmau@web.de>
2023-10-21 23:53:57 +0200

    add footer and hero section

---
+++ wahlentwicklung-frontend/src/app/sites/test-area/test-area.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-test-area',
+  templateUrl: './test-area.component.html',
+  styleUrls: []
+})
+export class TestAreaComponent {
+
+}

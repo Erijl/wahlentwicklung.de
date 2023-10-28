29d2fc9087e2a35a46835e3cc6891606e71d7e27
Erijl <justusmau@web.de>
2023-10-28 20:31:20 +0200

    dockerize frontend

--- wahlentwicklung-frontend/src/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  supabaseUrl: 'https://yunvtkoxdvyltqdinama.supabase.co',
-  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl1bnZ0a294ZHZ5bHRxZGluYW1hIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTYwMTcyMDYsImV4cCI6MjAxMTU5MzIwNn0.XdzgY0H1z_Bvmertb9FiaL8RqXtPWduTxt7tVdXic7k',
-}

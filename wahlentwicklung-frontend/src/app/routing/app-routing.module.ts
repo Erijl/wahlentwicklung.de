d9b2078b1964c65c155219e140f6638310db1921
Erijl <justusmau@web.de>
2023-11-26 22:19:49 +0100

    remove test area

--- wahlentwicklung-frontend/src/app/routing/app-routing.module.ts
+++ wahlentwicklung-frontend/src/app/routing/app-routing.module.ts
@@ -2,11 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import {LandingpageComponent} from "../sites/landingpage/landingpage.component";
-import {TestAreaComponent} from "../sites/test-area/test-area.component";
 
 const routes: Routes = [
   { path: '', component: LandingpageComponent },
-  { path: 'test', component: TestAreaComponent },
 ];
 
 @NgModule({

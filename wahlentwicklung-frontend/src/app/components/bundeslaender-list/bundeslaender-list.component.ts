459e22a9e3a7e26c9f8d4da7c9b1b2f43903f4e7
Erijl <justusmau@web.de>
2023-12-17 21:20:41 +0100

    changes for future backend

--- wahlentwicklung-frontend/src/app/components/bundeslaender-list/bundeslaender-list.component.ts
+++ wahlentwicklung-frontend/src/app/components/bundeslaender-list/bundeslaender-list.component.ts
@@ -11,18 +11,19 @@ export class BundeslaenderListComponent implements OnInit {
   bundeslaender: Bundesland[] = [];
   starredBundesland: Bundesland | null = null;
 
-  constructor(private dataService: DataService) {  }
+  constructor(private dataService: DataService) {
+  }
 
   ngOnInit() {
     this.getBundeslaender();
-    }
+  }
 
-    getBundeslaender(): void {
-        this.dataService.getBundeslaender().subscribe(bundeslaender => {
-            this.bundeslaender = bundeslaender;
-            console.log(bundeslaender);
-        });
-    }
+  getBundeslaender(): void {
+    this.dataService.getBundeslaender().subscribe(bundeslaender => {
+      this.bundeslaender = bundeslaender;
+      console.log(bundeslaender);
+    });
+  }
 
   starBundesland(bundesland: Bundesland) {
     // Handle starring and local storage here

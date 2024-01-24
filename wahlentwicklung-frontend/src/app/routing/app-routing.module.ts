636c8e2976af562bbd4c60e7bc0b17ccc6a3da37
Erijl <justusmau@web.de>
2024-01-24 10:44:36 +0100

    the big refactor v3

--- wahlentwicklung-frontend/src/app/routing/app-routing.module.ts
+++ wahlentwicklung-frontend/src/app/routing/app-routing.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import {LandingpageComponent} from "../sites/landingpage/landingpage.component";
+import { LandingpageComponent } from "../sites/landingpage/landingpage.component";
 
 const routes: Routes = [
-  { path: '', component: LandingpageComponent },
+  {path: '', component: LandingpageComponent},
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}

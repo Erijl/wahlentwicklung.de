a9c06523372c230fcea1ad06576e5dfbb2666c44
Erijl <justusmau@web.de>
2024-01-27 12:15:39 +0100

    fix application properties in container

--- wahlentwicklung-backend/src/main/java/com/erijl/wahlentwicklung/backend/config/CorsConfig.java
+++ wahlentwicklung-backend/src/main/java/com/erijl/wahlentwicklung/backend/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOriginPatterns("*") // Use allowedOriginPatterns instead of allowedOrigins
                 .allowedMethods("GET")
                 .allowedHeaders("*")
                 .allowCredentials(true);
